33d5487f8e1d48c6e4fc5ea96789757d99345e10
Breno Carneiro de Faria <breno@brenos-air.wireless.concordia.ca>
2025-02-01 17:55:19 -0500

    initial api established

---
+++ snappy/server.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	plaid "github.com/plaid/plaid-go/v31/plaid"
+)
+
+var environments = map[string]plaid.Environment{
+	"sandbox": plaid.Sandbox,
+}
+
+var (
+	PLAID_CLIENT_ID                      = ""
+	PLAID_SECRET                         = ""
+	PLAID_ENV                            = ""
+	PLAID_PRODUCTS                       = ""
+	PLAID_COUNTRY_CODES                  = ""
+	PLAID_REDIRECT_URI                   = ""
+	APP_PORT                             = ""
+	client              *plaid.APIClient = nil
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading the .env file %w", err)
+	}
+
+	PLAID_CLIENT_ID = os.Getenv("PLAID_CLIENT_ID")
+	PLAID_SECRET = os.Getenv("PLAID_SECRET")
+
+	if PLAID_CLIENT_ID == "" || PLAID_SECRET == "" {
+		log.Fatal("Error parsing the API keys from .env file")
+	}
+
+	PLAID_ENV = os.Getenv("PLAID_ENV")
+	PLAID_PRODUCTS = os.Getenv("PLAID_PRODUCTS")
+	PLAID_COUNTRY_CODES = os.Getenv("PLAID_COUNTRY_CODES")
+	PLAID_REDIRECT_URI = os.Getenv("PLAID_REDIRECT_URI")
+	APP_PORT = os.Getenv("APP_PORT")
+
+	// creating Plaid configuration
+	configuration := plaid.NewConfiguration()
+	configuration.AddDefaultHeader("PLAID-CLIENT-ID", PLAID_CLIENT_ID)
+	configuration.AddDefaultHeader("PLAID-SECRET", PLAID_SECRET)
+	configuration.UseEnvironment(environments[PLAID_ENV])
+	client = plaid.NewAPIClient(configuration)
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("api/hello_world", hello_world)
+	r.Run()
+}
+
+func hello_world(c *gin.Context) {
+
+	c.JSON(200, gin.H{
+		"mesage": "Hello World",
+	})
+}
