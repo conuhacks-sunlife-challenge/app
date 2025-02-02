39045c4bd5f1fd2d2e734c7eded2f6b08368d54e
Breno Carneiro de Faria <breno@brenos-air.wireless.concordia.ca>
2025-02-01 22:23:06 -0500

    new files added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './modules/home/index.tsx';
 import Dashboard from './modules/login/index.tsx';
 import MainMenu from './modules/dashboard/index.tsx';
+import PlaidIntegration from './modules/Link/index.tsx';
 
 const AppWrapper = () => {
   return (
@@ -10,6 +11,7 @@ const AppWrapper = () => {
         <Route path="/" element={<Home />} />
         <Route path="/create-account" element={<Dashboard />} />
         <Route path="/main-menu" element={<MainMenu />} />
+        <Route path="/plaid" element={<PlaidIntegration />} />
       </Routes>
     </Router>
   );
